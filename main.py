195cc6946a74c6e03343ee2e6a562e55f39acb76
perrebser <workperrebser@gmail.com>
2023-07-16 16:48:59 +0200

    game logic

--- main.py
+++ main.py
@@ -9,21 +9,39 @@ class TicTacToe:
         player_1 = input("Type name of the first player: ")
         player_2 = input("Type name of the second player: ")
         self.show_board()
-        if random.randint(0, 1) == 1:
+        self.play()
+
+    def play(self):
+        current_player = random.randint(1, 2)
+        if current_player == 1:
             print("Player 1 starts symbol -> X")
-            row = int(input("Type row  to mark: "))
-            col= int(input("Type column to mark: "))
-            self.make_move(row,col,1)
         else:
-            print("Player 2 starts symbol -> 0")
-            row=int(input("Type row  to mark: "))
-            col=int(input("Type column to mark: "))
-            self.make_move(row,col,2)
-    
+            print("Player 2 starts symbol -> O")
+        
+        while True:
+            row = int(input("Type row to mark: "))
+            col = int(input("Type column to mark: "))
+            self.make_move(row, col, current_player)
+            
+            if self.check_winner(current_player):
+                if current_player == 1:
+                    print("Player 1 wins!")
+                    self.player1_vic += 1
+                else:
+                    print("Player 2 wins!")
+                    self.player2_vic += 1
+                break
+            
+            current_player = 3 - current_player
+
     def show_board(self):
         for row in self.board:
             print(' '.join(row))
 
+    def check_winner(self,player):
+        symbol = 'X' if player == 1 else 'O'
+                
+        return False
     def make_move(self,row,col,player):
         if(self.board[row][col]=="-"):
             if(player==1):
