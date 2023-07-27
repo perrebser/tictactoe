992b758b952b699a1bd4b95f0b836e416eeeae4c
perrebser <workperrebser@gmail.com>
2023-07-27 20:38:29 +0200

    indices in board

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import random
 
+
 class TicTacToe:
     player1_vic = 0
     player2_vic = 0
     board = [['-' for _ in range(3)] for _ in range(3)]
-    
+
     def main(self):
         player_1 = input("Type name of the first player: ")
         player_2 = input("Type name of the second player: ")
@@ -17,12 +18,12 @@ class TicTacToe:
             print("Player 1 starts symbol -> X")
         else:
             print("Player 2 starts symbol -> O")
-        
+
         while True:
             row = int(input("Type row to mark: "))
             col = int(input("Type column to mark: "))
             self.make_move(row, col, current_player)
-            
+
             if self.check_winner(current_player):
                 if current_player == 1:
                     print("Player 1 wins!")
@@ -31,23 +32,28 @@ class TicTacToe:
                     print("Player 2 wins!")
                     self.player2_vic += 1
                 break
-            
+
             current_player = 3 - current_player
 
     def show_board(self):
-        for row in self.board:
-            print(' '.join(row))
+        print("   0   1   2")
+        for row, content in enumerate(self.board):
+            print(f"{row}  {' | '.join(content)}")
+            if row < 2:
+                print("  -----------")
 
-    def check_winner(self,player):
+    def check_winner(self, player):
+        solutions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 1, 2], [1, 4, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
         symbol = 'X' if player == 1 else 'O'
-                
+
         return False
-    def make_move(self,row,col,player):
-        if(self.board[row][col]=="-"):
-            if(player==1):
-                self.board[row][col]='X'
+
+    def make_move(self, row, col, player):
+        if (self.board[row][col] == "-"):
+            if (player == 1):
+                self.board[row][col] = 'X'
             else:
-                self.board[row][col]='0'
+                self.board[row][col] = '0'
 
         else:
             print("Esa posicion ya esta marcada!")
@@ -56,6 +62,7 @@ class TicTacToe:
     def show_stats(self):
         print("Victories of player 1: " + str(self.player1_vic))
         print("Victories of player 2: " + str(self.player2_vic))
+
 
 if __name__ == "__main__":
     game = TicTacToe()
